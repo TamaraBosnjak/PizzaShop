494604b6c656e7368bcecfee0da44a3512bb7fd8
TamaraBosnjak <bosnjakt@yahoo.com>
2023-10-31 15:30:33 +0100

    Ispravljene greske kod brojeva pita.

--- PizzaShop/Models/PieRepository.cs
+++ PizzaShop/Models/PieRepository.cs
@@ -6,7 +6,7 @@ namespace PizzaShop.Models
     {
         public List<Pie>AllPies { 
             get { return _pies; }
-            set {  _pies = value; }
+            set { _pies = value; }
         }
         public Pie GetPieById(int pieId)
         { 
@@ -22,8 +22,8 @@ namespace PizzaShop.Models
         public PieRepository()
         {
             Pie p1 = new Pie(1, "Pita s jabukama", "Mala pita sa jabukama", 1.25, false, true);
-            Pie p2 = new Pie(1, "Makovnjača", "Velika štrudla sa makom", 8.35, true, true);
-            Pie p3 = new Pie(1, "Burek", "Mastan burek sa sirom", 6.5, false, false);
+            Pie p2 = new Pie(2, "Makovnjača", "Velika štrudla sa makom", 8.35, true, true);
+            Pie p3 = new Pie(3, "Burek", "Mastan burek sa sirom", 6.5, false, false);
             _pies.Add(p1);
             _pies.Add(p2); 
             _pies.Add(p3);
