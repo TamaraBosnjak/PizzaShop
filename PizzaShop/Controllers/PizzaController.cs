74ed331692d56cf2ea6e3781149d0d885fb42b2f
TamaraBosnjak <bosnjakt@yahoo.com>
2023-11-05 17:17:21 +0100

    Kod nakon završenog 4.časa.

---
+++ PizzaShop/Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Models;
+
+namespace PizzaShop.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private IPieRepository _repository;
+        public PizzaController(IPieRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index(bool uslov, bool uslov2)
+        {
+            ViewBag.Uslov = uslov;
+            ViewBag.Uslov2 = uslov2;
+            ViewBag.Message = "Ovo je server-side poruka.";
+            ViewBag.Message2 = "Ovo je druga poruka.";
+          
+            return View();
+        }
+        public ViewResult List()
+        {
+            return View(_repository.AllPies);
+        }
+    }
+}
